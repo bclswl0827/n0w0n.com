71b8f757987691df094efeb4f7c716568f761410
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2025-01-20 02:22:07 +0800

    chore: Fix typos

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -2,7 +2,7 @@ import { Markdown } from "../../components/Markdown";
 
 const data = `An end-to-end fullstack developer with bachelor's degree, including the frontend, backend and hardware.
 
-This blog was created to share my practice and experience, you can get the source code [here](https://github.com/bclswl0827/n0w0n.com), it's built with TypeScript, React and Tailwind CSS. Previously I host my blog on \`ibcl.us\`, but the old one was closed due to the pressure of Ministry of State Security of China, beacuse one of the posts *hurt* them lol, **now this domain no longer belongs to me and it was bought by someone else for scam, so please be aware**.
+This blog was created to share my practice and experience, you can get the source code [here](https://github.com/bclswl0827/n0w0n.com), it's built with TypeScript, React and Tailwind CSS. Previously I host my blog on \`ibcl.us\`, but the old one was closed due to the pressure of Ministry of State Security of China, beacuse some of the posts *hurt* them lol, **now the old domain no longer belongs to me and it was bought by someone else for scam, so please be aware**.
 
 I usually like to play around with Linux and STM32/Arduino and some gadgets. BTW, I've been interested in seismology recently and have written some Go libraries libraries that fill a gap in this area.
 
