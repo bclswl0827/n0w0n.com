f7b36dd67ddf08cd3286bc55216556a2d750f847
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2025-01-20 02:26:15 +0800

    chore: Add some details

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -4,7 +4,7 @@ const data = `An end-to-end fullstack developer with bachelor's degree, includin
 
 This blog was created to share my practice and experience, you can get the source code [here](https://github.com/bclswl0827/n0w0n.com), it's built with TypeScript, React and Tailwind CSS. Previously I host my blog on \`ibcl.us\`, but the old one was closed due to the pressure of Ministry of State Security of China, beacuse some of the posts *hurt* them lol, **now the old domain no longer belongs to me and it was bought by someone else for scam, so please be aware**.
 
-I usually like to play around with Linux and STM32/Arduino and some gadgets. BTW, I've been interested in seismology recently and have written some Go libraries libraries that fill a gap in this area.
+I usually like to play around with Linux and STM32 / Arduino and some gadgets. I used to be obsessed with radio technology, but now I'm too busy to pay attention to it. BTW, I've been interested in seismology recently and have written some Go libraries libraries that fill a gap in this field.
 
 Finally, I hope this blog can bring you some joy and you can find something useful here.
 
