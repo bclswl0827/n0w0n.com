a0fb8ba05bd3b1fb85352b1de3f35e685bf0adef
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2025-01-21 02:50:56 +0800

    chore: Add friend links

--- src/config/friends.tsx
+++ src/config/friends.tsx
@@ -14,5 +14,15 @@ export const FriendsConfig: IFriendsConfig[] = [
 		name: "DongGua Blog",
 		url: "https://dgtea.site",
 		description: `Amateur radio operator (callsign: BD7PZX) and occasional photographer, passionate about cycling, traveling, and dabbling in coding`
+	},
+	{
+		name: " Airyu's Site",
+		url: "https://qiuyuair.com",
+		description: "Amateur radio operator (BG5VCU, AK6CU, VK2VCU, VK2/AK6CU), PC enthusiast."
+	},
+	{
+		name: "Sky Blond",
+		url: "https://www.skyblond.info",
+		description: "Miscellaneous information on life, coding, and photography."
 	}
 ];
