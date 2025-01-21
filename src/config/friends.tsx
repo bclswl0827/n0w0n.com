d4136cc5024d6fd218628002bff45ed7ee123df8
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2025-01-22 03:51:16 +0800

    chore: Add more friend links

--- src/config/friends.tsx
+++ src/config/friends.tsx
@@ -18,11 +18,31 @@ export const FriendsConfig: IFriendsConfig[] = [
 	{
 		name: " Airyu's Site",
 		url: "https://qiuyuair.com",
-		description: "Amateur radio operator (BG5VCU, AK6CU, VK2VCU, VK2/AK6CU), PC enthusiast."
+		description: `Amateur radio operator (callsign: BG5VCU, AK6CU, VK2VCU, VK2/AK6CU), PC enthusiast.`
 	},
 	{
 		name: "Sky Blond",
 		url: "https://www.skyblond.info",
-		description: "Miscellaneous information on life, coding, and photography."
+		description: `Miscellaneous information on life, coding, and photography.`
+	},
+	{
+		name: "ZRM's Blog",
+		url: "https://zrmblog.com",
+		description: `Amateur radio operator (callsign: BG7XSZ), mainly focuses on amateur radio, writing Linux tutorials and DevOps.`
+	},
+	{
+		name: "FKUN's Blog",
+		url: "https://fkun.tech",
+		description: `Currently living in Germany, focusing on technology and sharing daily life.`
+	},
+	{
+		name: "Ca1s1'Blog",
+		url: "https://vuln.top",
+		description: `Post 90s, mainly share CTF writeups and code audit records.`
+	},
+	{
+		name: "Radiofun-Hong's Blog",
+		url: "https://www.rhba7jwe.top",
+		description: `Amateur radio operator (callsign: BA7JWE), enthusiastic about amateur radio, television, flags, animation, and technology.`
 	}
 ];
