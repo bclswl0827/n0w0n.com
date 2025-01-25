a1885d7d80656507781ce50aa4dbe415a85bb039
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2025-01-26 00:52:40 +0800

    style: Fixed width for skeleton

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -9,15 +9,15 @@ export const Skeleton = () => {
 	}, []);
 
 	return (
-		<div className="p-8 my-auto space-y-3 w-full animate-fade overflow-y-hidden max-h-screen">
+		<div className="p-8 my-auto space-y-3 max-w-3xl mx-auto px-10 animate-fade overflow-y-hidden max-h-screen">
 			{[...new Array(skeletonRows)].map((_, index) => (
 				<div key={index} className="space-y-3 animate-pulse">
-					<div className="h-2.5 bg-gray-200 rounded-full w-32 mb-4 dark:bg-gray-600"></div>
-					<div className="h-2 bg-gray-300 rounded-full dark:bg-gray-700" />
-					<div className="h-2 bg-gray-300 rounded-full dark:bg-gray-700" />
-					<div className="h-2 bg-gray-300 rounded-full dark:bg-gray-700" />
-					<div className="h-2 bg-gray-300 rounded-full dark:bg-gray-700" />
-					<div className="h-2 bg-gray-300 rounded-full dark:bg-gray-700" />
+					<div className="h-2.5 bg-gray-200 rounded-full w-32 mb-4 dark:bg-gray-400"></div>
+					<div className="h-2 bg-gray-300 rounded-full dark:bg-gray-400" />
+					<div className="h-2 bg-gray-300 rounded-full dark:bg-gray-400" />
+					<div className="h-2 bg-gray-300 rounded-full dark:bg-gray-400" />
+					<div className="h-2 bg-gray-300 rounded-full dark:bg-gray-400" />
+					<div className="h-2 bg-gray-300 rounded-full dark:bg-gray-400" />
 				</div>
 			))}
 		</div>
